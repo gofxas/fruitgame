978669a58c21f429a1e50c279cbf8f25c1f199ec
刘传 <769601722@qq.com>
2023-06-01 09:09:12 +0800

    update

--- dist/assets/js/sound.js
+++ dist/assets/js/sound.js
@@ -2,201 +2,201 @@ const sounds = {
     // 苹果
     Y101: new Howl({
         volume: .6,
-        src: '/assets/audio/Y101.mp3',
+        src: './assets/audio/Y101.mp3',
     }),
     // 橘子
     Y102: new Howl({
         volume: .6,
-        src: '/assets/audio/Y102.mp3',
+        src: './assets/audio/Y102.mp3',
     }),
     // 芒果
     Y103: new Howl({
         volume: .6,
-        src: '/assets/audio/Y103.mp3',
+        src: './assets/audio/Y103.mp3',
     }),
     // 铃铛
     Y104: new Howl({
         volume: .6,
-        src: '/assets/audio/Y104.mp3',
+        src: './assets/audio/Y104.mp3',
     }),
     // 西瓜
     Y105: new Howl({
         volume: .6,
-        src: '/assets/audio/Y105.mp3',
+        src: './assets/audio/Y105.mp3',
     }),
     // 双星
     Y106: new Howl({
         volume: .6,
-        src: '/assets/audio/Y106.mp3',
+        src: './assets/audio/Y106.mp3',
     }),
     // 77
     Y107: new Howl({
         volume: .6,
-        src: '/assets/audio/Y107.mp3',
+        src: './assets/audio/Y107.mp3',
     }),
     // bar
     Y108: new Howl({
         volume: .6,
-        src: '/assets/audio/Y108.mp3',
+        src: './assets/audio/Y108.mp3',
     }),
     // 大三元
     Y114: new Howl({
         volume: .6,
-        src: '/assets/audio/Y114.mp3',
+        src: './assets/audio/Y114.mp3',
     }),
     // 小三元
     Y115: new Howl({
         volume: .6,
-        src: '/assets/audio/Y115.mp3',
+        src: './assets/audio/Y115.mp3',
     }),
     // 大4喜
     Y117: new Howl({
         volume: .6,
-        src: '/assets/audio/Y117.mp3',
+        src: './assets/audio/Y117.mp3',
     }),
     // 纵横四海
     Y118: new Howl({
         volume: .6,
-        src: '/assets/audio/Y118.mp3',
+        src: './assets/audio/Y118.mp3',
     }),
     // 大满贯
     Y120: new Howl({
         volume: .6,
-        src: '/assets/audio/Y120.mp3',
+        src: './assets/audio/Y120.mp3',
     }),
     // 天女散花
     Y122: new Howl({
         volume: .6,
-        src: '/assets/audio/Y122.mp3',
+        src: './assets/audio/Y122.mp3',
     }),
     // 开火车
     Y124: new Howl({
         volume: .6,
-        src: '/assets/audio/Y124.mp3',
+        src: './assets/audio/Y124.mp3',
     }),
     // 送灯
     Y131: new Howl({
         volume: .6,
-        src: '/assets/audio/Y131.mp3',
+        src: './assets/audio/Y131.mp3',
     }),
     // 真可惜
     zhenkexi: new Howl({
         volume: .6,
-        src: '/assets/audio/zhenkexi.mp3',
+        src: './assets/audio/zhenkexi.mp3',
     }),
     // 中了
     zhongle: new Howl({
         volume: .6,
-        src: '/assets/audio/zhongle.mp3',
+        src: './assets/audio/zhongle.mp3',
     }),
     // 开始
     rotate_start: new Howl({
         volume: .6,
-        src: '/assets/audio/rotate_start.mp3',
+        src: './assets/audio/rotate_start.mp3',
     }),
     // 结束
     rotate_end: new Howl({
         volume: .6,
-        src: '/assets/audio/rotate_end.mp3',
+        src: './assets/audio/rotate_end.mp3',
     }),
     // 进行中
     rotating: new Howl({
         volume: .3,
-        src: '/assets/audio/rotating.mp3',
+        src: './assets/audio/rotating.mp3',
         loop: true,
     }),
     ding: new Howl({
         volume: .6,
-        src: '/assets/audio/ding.mp3',
+        src: './assets/audio/ding.mp3',
     }),
     alternate: new Howl({
         volume: .6,
-        src: '/assets/audio/alternate.mp3',
+        src: './assets/audio/alternate.mp3',
     }),
     hitLukly: new Howl({
         volume: .6,
-        src: '/assets/audio/hitLukly.mp3'
+        src: './assets/audio/hitLukly.mp3'
     }),
     // boo boom 
     boo: new Howl({
         volume: .6,
-        src: '/assets/audio/boo.mp3'
+        src: './assets/audio/boo.mp3'
     }),
 
     C01: new Howl({
         volume: .3,
-        src: '/assets/audio/C01.mp3'
+        src: './assets/audio/C01.mp3'
     }),
     // 
     C02: new Howl({
         volume: .6,
-        src: '/assets/audio/C02.mp3'
+        src: './assets/audio/C02.mp3'
     }),
 
     C03: new Howl({
         volume: .3,
-        src: '/assets/audio/C03.mp3'
+        src: './assets/audio/C03.mp3'
     }),
     // 
     C04: new Howl({
         volume: .6,
-        src: '/assets/audio/C04.mp3'
+        src: './assets/audio/C04.mp3'
     }),
 
     C05: new Howl({
         volume: .3,
-        src: '/assets/audio/C05.mp3'
+        src: './assets/audio/C05.mp3'
     }),
     // 
     C06: new Howl({
         volume: .6,
-        src: '/assets/audio/C06.mp3'
+        src: './assets/audio/C06.mp3'
     }),
 
     C07: new Howl({
         volume: .3,
-        src: '/assets/audio/C07.mp3'
+        src: './assets/audio/C07.mp3'
     }),
     // 
     C08: new Howl({
         volume: .6,
-        src: '/assets/audio/C08.mp3'
+        src: './assets/audio/C08.mp3'
     }),
 
     C09: new Howl({
         volume: .3,
-        src: '/assets/audio/C09.mp3'
+        src: './assets/audio/C09.mp3'
     }),
     // 
     C10: new Howl({
         volume: .6,
-        src: '/assets/audio/C10.mp3'
+        src: './assets/audio/C10.mp3'
     }),
 
     C11: new Howl({
         volume: .3,
-        src: '/assets/audio/C11.mp3'
+        src: './assets/audio/C11.mp3'
     }),
     // 
     C13: new Howl({
         volume: .6,
-        src: '/assets/audio/C13.mp3'
+        src: './assets/audio/C13.mp3'
     }),
     luklyFail: new Howl({
         volume: .6,
-        src: '/assets/audio/luklyFail.mp3'
+        src: './assets/audio/luklyFail.mp3'
     }),
     luklySuccess: new Howl({
         volume: .6,
-        src: '/assets/audio/luklySuccess.mp3'
+        src: './assets/audio/luklySuccess.mp3'
     }),
     loop3: new Howl({
         volume: .3,
-        src: '/assets/audio/3.mp3',
+        src: './assets/audio/3.mp3',
         loop: true,
     }),
     train: new Howl({
         volume: .3,
-        src: '/assets/audio/train.mp3',
+        src: './assets/audio/train.mp3',
     })
 }
